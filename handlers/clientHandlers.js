fc1cf69582cfb0dbc6379bf008016b136de2afbe
Kenny Vu <kennyvu@live.ca>
2020-07-14 16:10:51 -0400

    Exercise 2 done

--- handlers/clientHandlers.js
+++ handlers/clientHandlers.js
@@ -1,4 +1,51 @@
-const { v4: uuidv4 } = require('uuid');
-const { clients } = require('../data/clients');
+const { v4: uuidv4 } = require("uuid");
+const { clients } = require("../data/clients");
 
 // write your handlers here...
+
+const handleClients = (req, res) => {
+  res.status(200);
+  res.send(clients);
+};
+const handleClientId = (req, res) => {
+  const clientId = req.params.id;
+  const clientProfile = clients.find((client) => client.id === clientId);
+  res.status(200);
+  res.send(clientProfile);
+};
+const handleNewClient = (req, res) => {
+  let newClient = req.body;
+  let clientEmail = newClient.email;
+  let emailExists = false;
+  clients.forEach((client) => {
+    if (client.email === clientEmail) {
+      emailExists = true;
+    }
+  });
+  newClient.id = 123;
+  if (!emailExists) {
+    newClient.id = uuidv4();
+    clients.push(newClient);
+    res.status(201);
+    res.send(clients);
+  } else {
+    res.status(400);
+    res.send("This email already exists... ʘ︵ʘ");
+  }
+};
+
+const handleRemoveClient = (req, res) => {
+  const clientId = req.params.id;
+  const clientProfile = clients.find((client) => client.id === clientId);
+  const clientIndex = clients.indexOf(clientProfile);
+  clients.splice(clientIndex, 1);
+  res.status(200);
+  res.send(clients);
+};
+
+module.exports = {
+  handleClients,
+  handleClientId,
+  handleRemoveClient,
+  handleNewClient,
+};
--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const {
+  handleClients,
+  handleClientId,
+  handleRemoveClient,
+  handleNewClient,
+} = require("./handlers/clientHandlers");
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('public'))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
+  .get("/clients", handleClients) //endpoint to access a list of all the clients.
+  .get("/clients/:id", handleClientId) //endpoint to access one client based on its id.
+  .post("/clients", handleNewClient) //endpoint to add a new client.
+
+  .delete("/clients/:id", handleRemoveClient) //endpoint to delete a customer based on its id.
 
   .listen(8000, () => console.log(`Listening on port 8000`));
